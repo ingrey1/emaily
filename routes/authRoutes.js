fb8e85e811f6cf930d2accee790d6ed7061054d4
Jason Mortensen <ingrey1@gmail.com>
2019-08-31 05:33:58 -0700

    updated authRoutes

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -7,6 +7,15 @@ module.exports = app => {
 		passport.authenticate("google", { scope: ["profile", "email"] })
 	);
 
+	app.get(
+
+		'/api/logout', (req, res) => {
+			req.logout();
+			res.send(req.user)
+		}
+
+		)
+
 	// after user grants permission, redirected to this location, passport sends request to google, grabs info
 	app.get("/auth/google/callback", passport.authenticate("google"));
 };
